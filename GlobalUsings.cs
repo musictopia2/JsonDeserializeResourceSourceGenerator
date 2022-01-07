6d6dfcf08664755857a17dffc987748fbe074f83
Andy <musictopia2@hotmail.com>
2022-01-06 18:16:14 -0700

    Added summary. Also, fixed bugs where would not even attempt to generate files by fixing file corruption.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,3 +3,4 @@ global using CommonBasicLibraries.CollectionClasses;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using System.Collections.Immutable;
+global using CommonSourceGeneratorsHelpers;
--- MySourceGenerator.cs
+++ MySourceGenerator.cs
@@ -1,8 +1,10 @@
 ﻿namespace JsonDeserializeResourceSourceGenerator;
+[Generator]
 public class MySourceGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
+
         var firsts = context.AdditionalTextsProvider
            .Where(xx => Path.GetExtension(xx.Path) == ".json");
         var nexts = firsts.Collect();
@@ -27,9 +29,12 @@ public class MySourceGenerator : IIncrementalGenerator
     {
         var files = GetFiles(list);
         var name = $"{compilation.AssemblyName}.Resources";
+        
+        BasicList<string> classes = new();
         files.ForEach(file =>
         {
-            string className = file.FileName;
+            string className = file.FileName.Replace(" ", ""); //because class names cannot have spaces.  i like the idea of just ignoring them (well see how that goes when i test).
+            classes.Add(className);
             string fins = $"{className}.g.cs";
             string data = file.Content.Replace("\"", "\"\""); //since its not bad, use the standard string and not custom is fine this time.
             string source = @$"using zz = CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.JsonSerializers.SystemTextJsonStrings;
@@ -56,5 +61,98 @@ public static class {className}
 }}";
             context.AddSource(fins, source);
         });
+        if (classes.Count >= 5)
+        {
+            GenerateSummary(context, classes, name); //eventually may find another way to determine if a summary is necessary.
+        }
+    }
+    private void GenerateSummary(SourceProductionContext context, BasicList<string> list, string ns)
+    {
+        SourceCodeStringBuilder builder = new();
+        builder.WriteLine(w =>
+        {
+            w.Write("namespace ")
+            .Write(ns)
+            .Write(";");
+        })
+        .WriteLine("public static class SummaryClass")
+        .WriteCodeBlock(w =>
+        {
+            w.WriteLine("public static T GetResource<T>(string name)")
+            .WriteCodeBlock(w =>
+            {
+                GenerateRegularMethods(w, list, ns);
+            })
+            .WriteLine("public static async Task<T> GetResourceAsync<T>(string name)")
+            .WriteCodeBlock(w =>
+            {
+                GenerateAsyncMethods(w, list, ns);
+            });
+        });
+        context.AddSource("Summary.g", builder.ToString());
+    }
+    private void GenerateAsyncMethods(ICodeBlock w, BasicList<string> list, string ns)
+    {
+        foreach (var item in list)
+        {
+            w.WriteLine(w =>
+            {
+                w.Write("if (name == ")
+                .AppendDoubleQuote(w =>
+                {
+                    w.Write(item);
+                })
+                .Write(")");
+            })
+            .WriteCodeBlock(w =>
+            {
+                w.WriteLine(w =>
+                {
+                    w.Write("return await global::")
+                    .Write(ns)
+                    .Write(".")
+                    .Write(item)
+                    .Write(".GetResourceAsync<T>();");
+                });
+            });
+        }
+        WriteException(w);
+    }
+    private void WriteException(ICodeBlock w)
+    {
+        w.WriteLine(w =>
+        {
+            w.CustomExceptionLine(w =>
+            {
+                w.Write("Nothing found with name {name}");
+            });
+        });
+    }
+    private void GenerateRegularMethods(ICodeBlock w, BasicList<string> list, string ns)
+    {
+        foreach (var item in list)
+        {
+            w.WriteLine(w =>
+            {
+                w.Write("if (name == ")
+                .AppendDoubleQuote(w =>
+                {
+                    w.Write(item);
+                })
+                .Write(")");
+            })
+            .WriteCodeBlock(w =>
+            {
+                w.WriteLine(w =>
+                {
+                    w.Write("return global::")
+                    .Write(ns)
+                    .Write(".")
+                    .Write(item)
+                    .Write(".GetResource<T>();");
+                });
+            });
+        }
+        WriteException(w);
     }
 }
